e92893e544dc2945d39e59014cad76854655afb7
tjdgus <tjdgus7562@naver.com>
2022-04-17 00:58:54 +0900

    [정성현] place / post

--- src/main/java/spring/project/nyangmong/web/PageController.java
+++ src/main/java/spring/project/nyangmong/web/PageController.java
@@ -5,9 +5,73 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-
-    @GetMapping("/main")
-    public String main() {
+    @GetMapping("/mainPage")
+    public String mainPage() {
         return "pages/mainPage";
+    }
+
+    @GetMapping("/welcomePage")
+    public String welcomePage() {
+        return "pages/welcomePage";
+    }
+
+    @GetMapping("/jarangDetail")
+    public String jarangDetail() {
+        return "pages/post/jarangDetail";
+    }
+
+    @GetMapping("/jarangList")
+    public String jarangList() {
+        return "pages/post/jarangList";
+    }
+
+    @GetMapping("/jarangWriteForm")
+    public String jarangWriteForm() {
+        return "pages/post/jarangWriteForm";
+    }
+
+    @GetMapping("/noticeList")
+    public String main6() {
+        return "pages/post/noticeList";
+    }
+
+    @GetMapping("/noticeWriteForm")
+    public String noticeWriteForm() {
+        return "pages/place/noticeWriteForm";
+    }
+
+    @GetMapping("/activityList")
+    public String activityList() {
+        return "pages/place/activityList";
+    }
+
+    @GetMapping("/cafeList")
+    public String cafeList() {
+        return "pages/place/cafeList";
+    }
+
+    @GetMapping("/hospitalList")
+    public String hospitalList() {
+        return "pages/place/hospitalList";
+    }
+
+    @GetMapping("/hotelList")
+    public String hotelList() {
+        return "pages/place/hotelList";
+    }
+
+    @GetMapping("/outlineList")
+    public String outlineList() {
+        return "pages/place/outlineList";
+    }
+
+    @GetMapping("/placeDetail")
+    public String placeDetail() {
+        return "pages/place/placeDetail";
+    }
+
+    @GetMapping("/spotList")
+    public String spotList() {
+        return "pages/place/spotList";
     }
 }
