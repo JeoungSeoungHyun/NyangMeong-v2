a4ca51933f031db072a54b9a21b9b9b71b793a85
tjdgus <tjdgus7562@naver.com>
2022-04-17 18:56:40 +0900

    [황주원] 수정사항 변경

--- src/main/resources/static/js/joinform.js
+++ src/main/resources/static/js/joinform.js
@@ -70,3 +70,33 @@ function passwordSameCheck() {
         valid.password.msg = "패스워드가 동일하지 않습니다.";
     }
 }
+
+function readURL(input) {
+
+    console.log("버튼클릭함1");
+
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+
+            $('#cover').attr('src', e.target.result); //cover src로 붙여지고
+
+            $('#fileName').val(input.files[0].name); //파일선택 form으로 파일명이 들어온다
+
+        }
+
+        reader.readAsDataURL(input.files[0]);
+
+    }
+
+}
+
+$("#myFileUp").change(function() {
+
+    readURL(this);
+
+    console.log("이미지 바뀜?");
+
+});
