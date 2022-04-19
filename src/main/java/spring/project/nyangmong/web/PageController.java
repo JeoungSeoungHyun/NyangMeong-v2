158ccfd0df7a76db3f1a61a94531e68b86d7e59c
LV <whgodbs32191@gmail.com>
2022-04-19 17:13:27 +0900

    [조해윤] 네이버 맵 실행 전

--- src/main/java/spring/project/nyangmong/web/PageController.java
+++ src/main/java/spring/project/nyangmong/web/PageController.java
@@ -6,15 +6,34 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.RequiredArgsConstructor;
 import spring.project.nyangmong.domain.places.PlaceRepository;
 import spring.project.nyangmong.domain.places.Places;
+import spring.project.nyangmong.service.PlaceService;
 
+@RequiredArgsConstructor
 @Controller
 public class PageController {
+    private final PlaceService placeService;
+    private final PlaceRepository placeRepository;
 
     // 메인 페이지 맵
     @GetMapping({ "/", "main", "mainPage" })
-    public String main() {
+    public String main(Model model) {
+        List<Places> placesSpot = placeRepository.placeTop3("관광지");
+        model.addAttribute("placesSpot", placesSpot);
+
+        List<Places> placesHospital = placeRepository.placeTop3("동물병원");
+        model.addAttribute("placesHospital", placesHospital);
+
+        List<Places> placesCafe = placeRepository.placeTop3("식음료");
+        model.addAttribute("placesCafe", placesCafe);
+
+        List<Places> placesActivity = placeRepository.placeTop3("체험");
+        model.addAttribute("placesActivity", placesActivity);
+
+        List<Places> placesHotel = placeRepository.placeTop3("숙박");
+        model.addAttribute("placesHotel", placesHotel);
         return "pages/mainPage";
     }
 
