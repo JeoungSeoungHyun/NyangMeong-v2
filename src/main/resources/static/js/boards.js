77bc76c0e2203c987f4ae01cecfa468a4ba1457b
fwangjuwon <fwangjuwon@naver.com>
2022-05-12 22:17:10 +0900

    [황주원] 임시

--- src/main/resources/static/js/boards.js
+++ src/main/resources/static/js/boards.js
@@ -10,6 +10,11 @@ $("#btn-updateJarang").click((event) => {
     update();
 });
 
+//게시글 삭제
+   $("#btn-delete").click(() => {
+        deletePost();
+    });
+
 
 // 공지사항 쓰기 
 async function writeNotice() {
@@ -64,3 +69,22 @@ async function update() {
         alert("수정실패");
     }
 }
+
+
+//게시글 삭제 
+ async function deletePost() {
+        let boardsId = $("#boardsId").val();
+        let response = await fetch(`/s/api/boards/${boardsId}`, {
+            method: "DELETE" // delete는 body가 없다.
+        });
+        let responseParse = await response.json();
+
+        if (responseParse.code == 1) {
+            alert("삭제성공");
+            location.href = "/";
+        } else {
+            alert("삭제실패");
+        }
+    }
+
+   
--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -1,12 +1,13 @@
-async function deleteComment(commentId) {
-    let response = await fetch(`/s/api/comment/${commentId}`, {
-        method: "DELETE"
-    });
-    let responseParse = await response.json();
-    if (responseParse.code == 1) {
-        alert("삭제 성공");
-        location.reload(); // ajax + ssr
-    } else {
-        alert("삭제 실패");
+ //댓글 삭제
+    async function deleteComment(commentId) {
+        let response = await fetch(`/s/api/comment/${commentId}`, {
+            method: "DELETE"
+        });
+        let responseParse = await response.json();
+        if (responseParse.code == 1) {
+            alert("삭제 성공");
+            location.reload(); // ajax + ssr
+        } else {
+            alert("삭제 실패");
+        }
     }
-}
